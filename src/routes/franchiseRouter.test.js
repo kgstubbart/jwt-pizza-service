ada0a3bc964c94661ed5025c55b0bc7f0742982d
Kason Stubbart <kgstubbart@gmail.com>
2026-02-03 08:11:46 -0700

    set up beforeAll

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -1,7 +1,11 @@
 const request = require('supertest');
 const app = require('../service');
-
+const { setAuth } = require('./authRouter.js');
 const { Role, DB } = require('../database/database.js');
+
+let admin;
+let adminToken;
+let dinerToken;
 
 async function createAdminUser() {
   let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
@@ -15,3 +19,11 @@ async function createAdminUser() {
 function randomName() {
   return Math.random().toString(36).substring(2, 12);
 }
+
+beforeAll(async () => {
+    admin = await createAdminUser();
+    adminToken = await setAuth(admin);
+    const diner = { name: randomName(), email: randomName() + '@diner.com', password: 'dinerpassword' };
+    const registerRes = await request(app).post('/api/auth').send(diner);
+    dinerToken = registerRes.body.token;
+});
