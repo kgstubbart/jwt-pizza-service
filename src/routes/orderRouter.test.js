9690e89efa436694b3fcfc0f00eea73119f0c621
Kason Stubbart <kgstubbart@gmail.com>
2026-02-03 16:06:33 -0700

    skeleton for orderRouter.test.js

---
+++ src/routes/orderRouter.test.js
@@ -0,0 +1,37 @@
+const request = require("supertest");
+const app = require("../service");
+const { setAuth } = require("./authRouter.js");
+const { DB, Role } = require("../database/database.js");
+
+function randomName() {
+	return Math.random().toString(36).substring(2, 12);
+}
+
+async function createAdminUser() {
+	let user = { password: "toomanysecrets", roles: [{ role: Role.Admin }] };
+	user.name = randomName();
+	user.email = user.name + "@admin.com";
+
+	user = await DB.addUser(user);
+	return { ...user, password: "toomanysecrets" };
+}
+
+let admin;
+let adminToken;
+let diner;
+let dinerToken;
+
+beforeAll(async () => {
+	admin = await createAdminUser();
+	adminToken = await setAuth(admin);
+
+	diner = {
+		name: randomName(),
+		email: randomName() + "@diner.com",
+		password: "dinerpassword",
+	};
+	const registerRes = await request(app).post("/api/auth").send(diner);
+	dinerToken = registerRes.body.token;
+});
+
+
