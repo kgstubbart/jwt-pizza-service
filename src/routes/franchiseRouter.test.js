dacb5a7dd87fac8405c34f6ef5f2e8c290f9caff
Kason Stubbart <kgstubbart@gmail.com>
2026-02-03 08:06:49 -0700

    added in starter code

---
+++ src/routes/franchiseRouter.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const app = require('../service');
+
+const { Role, DB } = require('../database/database.js');
+
+async function createAdminUser() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  user = await DB.addUser(user);
+  return { ...user, password: 'toomanysecrets' };
+}
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
