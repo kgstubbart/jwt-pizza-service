bec72fc5373a83139a1e211fffaf92708a91e4c2
Kason Stubbart <kgstubbart@gmail.com>
2026-03-30 16:53:22 -0600

    port fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./service.js');
 
 const port = process.argv[2] || 80;
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server started on port ${port}`);
 });
