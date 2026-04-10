9310013e7b8282abfe715d7bb30ef9c1b70f1f76
Kason Stubbart <kgstubbart@gmail.com>
2026-04-09 20:48:44 -0600

    correct connections to metrics

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const app = require('./service.js');
 const metrics = require('./metrics');
 
 const port = process.argv[2] || 80;
-app.use((req, res, next) => metrics.requestTracker(req, res, next));
+
 metrics.start(5000);
+
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server started on port ${port}`);
 });
